31491b6f28e36acaa2c9c4dab414d770543cbfe5
kouakou n'dri <ndripros2019@gmail.com>
2022-09-09 04:28:59 -0700

    my first quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,and that piece of art is useful\" - dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
