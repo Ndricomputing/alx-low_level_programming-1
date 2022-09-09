679a724e5683b4dcfafd48053a91ef00a26a271d
kouakou n'dri <ndripros2019@gmail.com>
2022-09-09 02:12:41 -0700

    my first printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
 * main - print the string in the put function
 *
-* Description: using the main fuction
+* Description: using the main function
 * this program prints "programming is like building a multilangual puzzle
 * Return: 0
 */
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,\n");
-        Return: (0);
+        return: (0);
 }
