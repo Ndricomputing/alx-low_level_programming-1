65a05cc1df5c45d1894ff685b466f3b1eb038017
kouakou n'dri <ndripros2019@gmail.com>
2022-09-08 22:47:31 -0700

    my first puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+/**
+* main - print the string in the put fonction
+*
+* Description: using the main function
+*this program prints ''programming is like building a multilangual puzzle
+* Return: 0
+*/
+int main(void)
+{
+        puts("\"programming is like building a multilangual puzzle");
+        return (0);
+}
